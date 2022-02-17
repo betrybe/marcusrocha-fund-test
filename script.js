0c8e6f2cbd5ded395c58698f3f5bf7cbc6cf196c
Marcus Rocha <sif.marcusvinicius@gmail.com>
2022-02-16 21:03:53 -0300

    Corrigindo pontos indicados peloEslint

--- script.js
+++ script.js
@@ -1,30 +1,57 @@
 function refreshPrice(price) {
-  let tag = document.querySelector('.total-price');
-  tag.innerHTML=price;
+  const tag = document.querySelector('.total-price');
+  tag.innerHTML = price;
 }
 
 function sumPrice() {
   let totalPrice = 0;
-  if (localStorage.getItem('products') != null){
+  if (localStorage.getItem('products') != null) {
     const lsProducts = JSON.parse(localStorage.getItem('products'));
-    for (let index = 0; index < lsProducts.length; index++){
-      const salePrice = lsProducts[index].salePrice;
+
+    lsProducts.forEach((item) => {
+      const [salePrice] = [item.salePrice];
       totalPrice += salePrice;
-    }
+    });
   }
   refreshPrice(totalPrice);
 }
 
-function verifyLocalStorage() {
-  if (localStorage.getItem('products') != null){
+async function cartItemClickListener(event) {
+  const product = event.srcElement.innerText;
+  const sku = product.split(' ', 2)[1];
+  console.log(sku);
+  
+  if (localStorage.getItem('products') != null) {
     const lsProducts = JSON.parse(localStorage.getItem('products'));
-    for (let index = 0; index < lsProducts.length; index++){
-      const itemStorage = lsProducts[index];
+
+    lsProducts.forEach((item, index) => {
+      if (item.sku === sku) {
+        lsProducts.splice(index, 1);
+      }
+    });
+    localStorage.setItem('products', JSON.stringify(lsProducts));
+    sumPrice();
+  }
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+function verifyLocalStorage() {
+  if (localStorage.getItem('products') != null) {
+    const lsProducts = JSON.parse(localStorage.getItem('products'));
+
+    lsProducts.forEach((itemStorage) => {
       const [sku, name, salePrice] = [itemStorage.sku, itemStorage.name, itemStorage.salePrice];
       const item = document.querySelector('ol');
       const newItem = createCartItemElement({ sku, name, salePrice });
-      item.appendChild(newItem);  
-    }
+      item.appendChild(newItem);
+    });
   }
   sumPrice();
 }
@@ -32,10 +59,10 @@ function verifyLocalStorage() {
 function localStorageManage({ sku, name, salePrice }) {
   const lsProducts = JSON.parse(localStorage.getItem('products')) || [];
   const product = {
-    'sku': sku,
-    'name': name,
-    'salePrice': salePrice
-  }
+    sku,
+    name,
+    salePrice,
+  };
   lsProducts.push(product);
   localStorage.setItem('products', JSON.stringify(lsProducts));
   sumPrice();
@@ -56,7 +83,7 @@ function removeLoad() {
 async function apiAccess(urlAPI, search) {
   await createLoad();
   const result = await fetch(`${urlAPI}${search}`);
-  const resultJson =  await result.json();
+  const resultJson = await result.json();
   await removeLoad();
   return resultJson;
 }
@@ -80,36 +107,16 @@ function removeAllItensCart() {
   while (elements.length > 0) elements[0].remove();
 }
 
-async function cartItemClickListener(event) {
-  const product = event.srcElement.innerText;
-  let sku = product.split(' ', 2)[1];
-  if (localStorage.getItem('products') != null){
-    const lsProducts = JSON.parse(localStorage.getItem('products'));
-    for (let index = 0; index < lsProducts.length; index++){
-      if (lsProducts[index].sku === sku){
-        lsProducts.splice(index, 1);
-      }
-    }
-    localStorage.setItem('products', JSON.stringify(lsProducts));
-    await removeAllItensCart()
-    verifyLocalStorage();
-  }
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
 }
 
 async function addProductItem(search) {
   const urlAPI = 'https://api.mercadolibre.com/items/';
   const resultJson = await apiAccess(urlAPI, search);
   const [sku, name, salePrice] = [resultJson.id, resultJson.title, resultJson.price];
-  let item = document.querySelector('ol');
-  let newItem = createCartItemElement({ sku, name, salePrice });
+  const item = document.querySelector('ol');
+  const newItem = createCartItemElement({ sku, name, salePrice });
   item.appendChild(newItem);
   localStorageManage({ sku, name, salePrice });
 }
@@ -135,32 +142,30 @@ function createProductItemElement({ sku, name, image }) {
 
 async function insertItems(resultJson) {
   const items = document.querySelector('.items');
-  for (let index = 0; index < resultJson.results.length; index++) {
-    const item = resultJson.results[index];
+
+  resultJson.results.forEach((item) => {
     const [sku, name, image] = [item.id, item.title, item.thumbnail];
     const sectionNew = (createProductItemElement({ sku, name, image }));
     items.appendChild(sectionNew);
-  }
+  });
 }
 
-async function removeAllItems(){
-  if (localStorage.getItem('products') != null){
-    await removeAllItensCart()
+async function removeAllItems() {
+  if (localStorage.getItem('products') != null) {
+    await removeAllItensCart();
     localStorage.removeItem('products');
     verifyLocalStorage();
   }
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-async function load(){
+async function load() {
   const search = 'Computador';
-  const urlAPI = `https://api.mercadolibre.com/sites/MLB/search?q=`;
+  const urlAPI = 'https://api.mercadolibre.com/sites/MLB/search?q=';
   const resultJson = await apiAccess(urlAPI, search);
   insertItems(resultJson);
   verifyLocalStorage();
 }
 
-window.onload = () => { load() };
+window.onload = () => { 
+  load(); 
+};
